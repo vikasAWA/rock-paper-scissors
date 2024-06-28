1557ea86b530a5b2cf543848f4e138aa03cd64cd
vikasAWA <85212839+vikasAWA@users.noreply.github.com>
2024-06-28 23:57:46 +0530

    Startinf from scratch

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-let buttonContainer = document.querySelector(".container")
-
-
-function getPlayerChoice() {}
-    buttonContainer.addEventListener('click', (e) => {
-        let output = document.querySelector(".text");
-        output.textContent = `You chose ${e.target.id}`;
-    })
 
