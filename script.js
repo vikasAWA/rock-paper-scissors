f6c1bddef347c829cf4c84b817e218391ab2d5b6
vikasAWA <85212839+vikasAWA@users.noreply.github.com>
2024-06-29 19:19:54 +0530

    Added playRound function
    Added a function to play one round that is called
    when an image is clicked.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const container = document.querySelector("#container");
 const computer = document.querySelector("#computer");
 const human = document.querySelector("#human");
+const images = ['rock', 'paper', 'scissor']
+
 
 function addImageElement() {
-    let images = ['rock', 'paper', 'scissor']
     for (let player of [computer, human]) {
         for (let image of images) {
             let img = document.createElement('img');
@@ -11,5 +12,41 @@ function addImageElement() {
             img.alt = image + " pic";
             player.appendChild(img);
         }
+    }
+}
+addImageElement();
+
+function getComputerChoice() {
+    return images[Math.floor(Math.random() * 3)]
+}
+
+
+human.addEventListener("click", (e) => {
+    let humanChoice = e.target.alt.split(" ")[0];
+    let computerChoice = getComputerChoice();
+    console.log(computer.childNodes)
+    playRound(humanChoice, computerChoice);
+})
+
+
+function playRound(humanSelection, computerSelection) {
+    let roundResult = document.querySelector(".text");
+    switch (humanSelection + computerSelection) {
+        case "rockpaper":
+        case "paperscissor":
+        case "scissorrock":
+            roundResult.textContent = "You lose this round!";
+            break;
+        case "paperrock":
+        case "scissorpaper":
+        case "rockscissor":
+            roundResult.textContent = "You win this round!";
+            break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorscissor":
+            roundResult.textContent = "This round is a tie!"
+            break;
+
     }
 }
