c9e7f7a7ed295d2a601bd863ad86ba866f35429d
vikasAWA <85212839+vikasAWA@users.noreply.github.com>
2024-06-29 16:48:01 +0530

    Add a function addImageElement
    this function fill html file with images in desired
    container.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const container = document.querySelector("#container");
+const computer = document.querySelector("#computer");
+const human = document.querySelector("#human");
 
+function addImageElement() {
+    let images = ['rock', 'paper', 'scissor']
+    for (let player of [computer, human]) {
+        for (let image of images) {
+            let img = document.createElement('img');
+            img.src =  "images/" + image + ".png"
+            img.alt = image + " pic";
+            player.appendChild(img);
+        }
+    }
+}
