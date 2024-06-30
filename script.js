3bbf0c1ba4dba0a6600b50ea69dbf7cb6ef30c06
vikasAWA <85212839+vikasAWA@users.noreply.github.com>
2024-06-30 18:36:03 +0530

    Added highlight feature and live score feature
    Modified html, css and script file to add the
    highlight feature in the game, which highlight the
    clicked element. Also add live score counter to the
    display. Also added a function that announce the
    winner once points 5 is reached for a player.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const container = document.querySelector("#container");
 const computer = document.querySelector("#computer");
 const human = document.querySelector("#human");
 const images = ['rock', 'paper', 'scissor']
+let humanScore = 0;
+let computerScore = 0;
 
 
 function addImageElement() {
@@ -22,29 +24,79 @@ function getComputerChoice() {
 
 
 function playRound(humanSelection, computerSelection) {
-    let roundResult = document.querySelector(".text");
+    let roundResult = document.querySelector(".result");
+
     switch (humanSelection + computerSelection) {
         case "rockpaper":
         case "paperscissor":
         case "scissorrock":
-            roundResult.textContent = "You lose this round!";
+            roundResult.textContent = `Computer choice is ${computerSelection}. Your Choice is ${humanSelection}. You lose this round!`;
+            computerScore += 1;
             break;
         case "paperrock":
         case "scissorpaper":
         case "rockscissor":
-            roundResult.textContent = "You win this round!";
+            roundResult.textContent = `Computer choice is ${computerSelection}. Your Choice is ${humanSelection}. You win this round!`;
+            humanScore += 1;
             break;
         case "rockrock":
         case "paperpaper":
         case "scissorscissor":
-            roundResult.textContent = "This round is a tie!"
+            roundResult.textContent = `Computer choice is ${computerSelection}. Your Choice is ${humanSelection}. it's a tie.!`;
             break;
 
     }
+}
+
+function getWinner() {
+    let winner = document.createElement('div');
+    winner.classList.add('winner');
+    document.body.appendChild(winner);
+    winner.setAttribute('style', "color: green; font-size: 100px; text-align: center; margin-top: 300px");
+    if (humanScore === 5) {
+        winner.textContent = "You are the Winner!.";
+        container.remove();
+    }
+    else if (computerScore === 5) {
+        winner.textContent = "Computer is the Winner!";
+        container.remove();
+    }
+
 }
 
 human.addEventListener("click", (e) => {
     let humanChoice = e.target.alt.split(" ")[0];
     let computerChoice = getComputerChoice();
     playRound(humanChoice, computerChoice);
+    
+    // Adding highlight feature for human choice
+    human.childNodes.forEach(element => {
+        if (element === e.target) {
+            element.classList.add('highlight');
+        }
+        else {
+            element.classList.remove('highlight');
+        }
+    });
+
+    // Adding highligh feature for computer choice
+    computer.childNodes.forEach(element => {
+        if (element.alt.split(" ")[0] === computerChoice) {
+            element.classList.add('highlight');
+        }
+        else {
+            element.classList.remove('highlight');
+        }
+    })
+  
+
+    let scoreComputer = document.querySelector('.computerScore');
+    let scoreHuman = document.querySelector('.humanScore');
+    scoreComputer.textContent = "Computer: " + computerScore;
+    scoreHuman.textContent = "Human: " + humanScore;
+    getWinner()
 })
+
+
+
+
